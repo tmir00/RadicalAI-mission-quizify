2ea662c34b7955ea9940fe700a003828b9541a4f
tmir <taahaa.mir@mail.mcgill.ca>
2024-05-10 00:17:52 -0400

    Completed Task 4

--- tasks/task_4/task_4.py
+++ tasks/task_4/task_4.py
@@ -1,6 +1,7 @@
 # embedding_client.py
 
 from langchain_google_vertexai import VertexAIEmbeddings
+import streamlit as st
 
 class EmbeddingClient:
     """
@@ -34,7 +35,9 @@ class EmbeddingClient:
         # Read about the VertexAIEmbeddings wrapper from Langchain here
         # https://python.langchain.com/docs/integrations/text_embedding/google_generative_ai
         self.client = VertexAIEmbeddings(
-            #### YOUR CODE HERE ####
+            model_name = model_name,
+            project = project,
+            location = location
         )
         
     def embed_query(self, query):
@@ -62,11 +65,12 @@ class EmbeddingClient:
 
 if __name__ == "__main__":
     model_name = "textembedding-gecko@003"
-    project = "YOUR PROJECT ID HERE"
+    project = "sample-mission-422802"
     location = "us-central1"
 
     embedding_client = EmbeddingClient(model_name, project, location)
     vectors = embedding_client.embed_query("Hello World!")
     if vectors:
+        st.write(vectors)
         print(vectors)
         print("Successfully used the embedding client!")
